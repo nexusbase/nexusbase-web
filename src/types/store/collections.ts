829de23ba538542eb8f1d08aa9e118f703207e2f
Alex <iamalexchip@gmail.com>
2020-11-12 09:20:47 +0200

    feat: Field editor

--- src/types/store/collections.ts
+++ src/types/store/collections.ts
@@ -4,6 +4,7 @@ export default interface ICollectionsState {
   isFetchingOne: boolean,
   isFetchingList: boolean,
   isCreating: boolean,
+  isUpdating: boolean,
   relatedCollections: ICollectionModel[],
   collections: ICollectionModel[],
   collection: null | ICollectionModel,
@@ -16,6 +17,8 @@ export enum ICollectionActionTypes {
   GET_COLLECTIONS_SUCCESS = 'GET_COLLECTIONS_SUCCESS',
   GET_COLLECTION_START = 'GET_COLLECTION_START',
   GET_COLLECTION_SUCCESS = 'GET_COLLECTION_SUCCESS',
+  UPDATE_FIELD_START = 'UPDATE_FIELD_START',
+  UPDATE_FIELD_SUCCESS = 'UPDATE_FIELD_SUCCESS',
   CLEAR_COLLECTIONS = 'CLEAR_COLLECTIONS',
   CLEAR_COLLECTION = 'CLEAR_COLLECTION',
 }
@@ -54,6 +57,18 @@ export interface GetCollectionSuccess {
   }
 }
 
+export interface UpdateFieldStart {
+  type: ICollectionActionTypes.UPDATE_FIELD_START
+}
+
+export interface UpdateFieldSuccess {
+  type: ICollectionActionTypes.UPDATE_FIELD_SUCCESS;
+  payload: {
+    collection: ICollectionModel;
+    relatedCollections: ICollectionModel[];
+  }
+}
+
 export interface ClearCollections {
   type: ICollectionActionTypes.CLEAR_COLLECTIONS;
 }
@@ -69,5 +84,7 @@ export type ICollectionsAction =
   | GetCollectionsSuccess
   | GetCollectionStart
   | GetCollectionSuccess
+  | UpdateFieldStart
+  | UpdateFieldSuccess
   | ClearCollections
   | ClearCollection;
